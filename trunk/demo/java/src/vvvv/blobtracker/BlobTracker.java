962e9f01481f05760e923eb114dabfb348876a5f
Michael.Nischt <Michael.Nischt@2735173b-5428-0410-ba1d-dbb0dc069ffb>
2007-02-05 17:48:14 +0000

    Added Basic source for the TouchIt side-event demo.

---
+++ trunk/demo/java/src/vvvv/blobtracker/BlobTracker.java
@@ -0,0 +1,69 @@
+/*
+ * jens wunderling
+ * http://www.sport4minus.de/blog/?p=226
+ */
+
+package vvvv.blobtracker;
+
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
+
+import processing.core.PApplet;
+import processing.core.PFont;
+
+public class BlobTracker extends PApplet {
+    PFont font;
+    
+    Cursor[] myCursors;
+    
+    Tracker myTracker;
+    
+    static int WIDTH = 640;//1024;
+    static int HEIGHT = 480;//768;
+    
+    public void setup() {
+        background(0);
+        ellipseMode(CENTER);
+        rectMode(CENTER);
+        size(WIDTH, HEIGHT, OPENGL);
+        frameRate(120);
+        
+        myCursors = new Cursor[10];
+        myTracker = new Tracker(myCursors);
+        
+        fill(255);
+        font = loadFont("Beatbox-10.vlw");
+        textFont(font, 10);
+        
+        smooth();
+    }
+    
+    public void draw() {
+        background(0);
+        myTracker.checkGlobs();
+        myTracker.draw(g);
+        //myTracker.clearBlobs();
+        
+        for (int i = 0; i < myCursors.length; i++) {
+            if (myCursors[i] != null) {
+                myCursors[i].draw(g);
+            }
+        }
+        
+    }
+    
+    static public void main(String args[])  {
+        
+        try {
+            FileOutputStream fos = new FileOutputStream("d:\\errors.txt");
+            PrintStream ps = new PrintStream(fos);
+            System.setErr(ps);
+            System.setOut(ps);
+            
+            
+            PApplet.main(new String[] { "vvvv.blobtracker.BlobTracker" });
+            
+        } catch(Exception e) {}
+    }
+}
