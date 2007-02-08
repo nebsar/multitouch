3d1c2bf4343adcffcd1645cc7f3a405f289b3e94
Michael.Nischt <Michael.Nischt@2735173b-5428-0410-ba1d-dbb0dc069ffb>
2007-02-08 16:15:12 +0000

    Friday demo done

--- demo/java/src/de/telekom/laboratories/multitouch/demo/FingerPaint.java
+++ demo/java/src/de/telekom/laboratories/multitouch/demo/FingerPaint.java
@@ -67,7 +67,7 @@ public class FingerPaint {
         private float g;
         private float b;
         
-        private float radius = 0.02f;
+        private float radius = 0.04f;
         
         // </editor-fold>
         
@@ -153,6 +153,18 @@ public class FingerPaint {
         
         // </editor-fold>
         
+        // <editor-fold defaultstate="collapsed" desc=" Id ">
+        
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+        
+        // </editor-fold>
+        
         private void draw(final GL gl) {
             
             final float x = getX(), y = getY(), radius = getRadius();
@@ -204,29 +216,27 @@ public class FingerPaint {
             final float perpX = - dY;
             final float perpY =   dX;
             
-            
-            gl.glPointSize(4.0f);
             gl.glBegin(GL.GL_QUADS);
             
+                gl.glVertex3f(x+perpX, y+perpY, 0.5f);
+                gl.glVertex3f(x-perpX, y-perpY, 0.5f);
+                
+                gl.glVertex3f(oldX-perpX, oldY-perpY, 0.5f);
+                gl.glVertex3f(oldX+perpX, oldY+perpY, 0.5f);
+            
+//            if(RANDOM.nextBoolean()) {            
+//                gl.glVertex3f(x-perpX, y-perpY, 0.5f);
+//                gl.glVertex3f(x+perpX, y+perpY, 0.5f);
+//                                
+//                gl.glVertex3f(oldX-perpX, oldY-perpY, 0.5f);
+//                gl.glVertex3f(oldX+perpX, oldY+perpY, 0.5f);
+//            } else {
 //                gl.glVertex3f(x+perpX, y+perpY, 0.5f);
 //                gl.glVertex3f(x-perpX, y-perpY, 0.5f);
-//                
-//                gl.glVertex3f(oldX-perpX, oldY-perpY, 0.5f);                
+//              
 //                gl.glVertex3f(oldX+perpX, oldY+perpY, 0.5f);
-            
-            if(RANDOM.nextBoolean()) {            
-                gl.glVertex3f(x-perpX, y-perpY, 0.5f);
-                gl.glVertex3f(x+perpX, y+perpY, 0.5f);
-                                
-                gl.glVertex3f(oldX-perpX, oldY-perpY, 0.5f);                
-                gl.glVertex3f(oldX+perpX, oldY+perpY, 0.5f);            
-            } else {
-                gl.glVertex3f(x+perpX, y+perpY, 0.5f);
-                gl.glVertex3f(x-perpX, y-perpY, 0.5f);
-              
-                gl.glVertex3f(oldX+perpX, oldY+perpY, 0.5f);                
-                gl.glVertex3f(oldX-perpX, oldY-perpY, 0.5f);                
-            }
+//                gl.glVertex3f(oldX-perpX, oldY-perpY, 0.5f);
+//            }
                             
             gl.glEnd();
             
@@ -236,6 +246,120 @@ public class FingerPaint {
         
         private static final Random RANDOM = new Random();
 
+
+        
+    }
+    
+    // </editor-fold>
+    
+    // <editor-fold defaultstate="collapsed" desc=" Slider ">
+    
+    private static class Slider {
+        
+        // <editor-fold defaultstate="collapsed" desc=" Attributes ">
+        
+        private int id;
+        
+        // center of the brush
+        private float x;
+        private float y;
+        //color of brush
+        private float extendX = 0.075f;
+        private float extendY = 0.250f;
+        // slider state
+        private  float factor = 0.5f;
+        
+        // </editor-fold>
+        
+        // <editor-fold defaultstate="collapsed" desc=" Constructors ">
+        
+        private static int ids = 0;
+        public Slider() {
+            id = ids++;
+        }
+        
+        public Slider(float x, float y) {
+            this();
+            setX(x);
+            setY(y);
+        }
+        
+        // </editor-fold>
+        
+        // <editor-fold defaultstate="collapsed" desc=" Location ">
+        
+        public float getX() {
+            return x;
+        }
+        
+        public void setX(float x) {
+            this.x = x;
+        }
+        
+        public float getY() {
+            return y;
+        }
+        
+        public void setY(float y) {
+            this.y = y;            
+        }
+        
+        public void setLocation(float x, float y) {
+            setX(x);
+            setY(y);
+        }
+        
+        // </editor-fold>
+        
+        // <editor-fold defaultstate="collapsed" desc=" Extend & Size ">
+
+        public float getExtendX() {
+            return extendX;
+        }
+
+        private void setExtendX(float extendX) {
+            this.extendX = Math.abs(extendX);
+        }
+
+        private float getExtendY() {
+            return extendY;
+        }
+
+        private void setExtendY(float extendY) {
+            this.extendY = Math.abs(extendY);
+        }
+        
+        private void setExtend(float x, float y)
+        {
+            setExtendX(x);
+            setExtendY(y);
+        }
+        
+        private float getWidth() {
+            return getExtendX()*2.0f;
+        }
+        
+        public void setWidth(float width) {
+            setExtendX(width/2.0f);
+        }
+        
+        private float getHeight() {
+            return getExtendY()*2.0f;
+        }        
+        
+        public void setHeight(float height) {
+            setExtendY(height/2.0f);
+        }
+        
+        public void setSize(float width, float height) {
+            setWidth(width);
+            setHeight(height);            
+        }
+        
+        // </editor-fold>
+        
+        // <editor-fold defaultstate="collapsed" desc=" Id ">
+        
         public int getId() {
             return id;
         }
@@ -244,9 +368,119 @@ public class FingerPaint {
             this.id = id;
         }
         
+        // </editor-fold>        
+        
+        // <editor-fold defaultstate="collapsed" desc=" Factor ">
+        
+        public float getFactor() {
+            return factor;
+        }
+        
+        public void setFactor(float factor) {
+            this.factor = Math.max(0.0f, Math.min(factor, 1.0f));
+            //System.out.println(id + ": " + factor);
+        }
+        
+        // </editor-fold>    
+        
+        private void draw(final GL gl) {
+            
+            final float x = getX(), y = getY();
+            final float extX = getExtendX(), extY = getExtendY();
+
+            //gl.glLineStipple(1, (short)0xFFFF);
+            gl.glLineWidth(2.0f);
+            
+            gl.glBegin(GL.GL_QUADS);
+            
+            gl.glColor3f(0.0f, 0.0f, 0.0f);
+            gl.glVertex3f(x-extX, y+extY, 0.35f);
+            gl.glVertex3f(x-extX, y-extY, 0.35f);
+            gl.glVertex3f(x+extX, y-extY, 0.35f);
+            gl.glVertex3f(x+extX, y+extY, 0.35f);
+            
+            gl.glEnd();            
+            
+            gl.glPolygonMode(GL.GL_FRONT_AND_BACK, GL.GL_LINE);
+            
+            gl.glBegin(GL.GL_QUADS);
+            
+            gl.glColor3f(0.66f, 0.66f, 0.66f);
+            gl.glVertex3f(x-extX, y+extY, 0.25f);
+            gl.glVertex3f(x-extX, y-extY, 0.25f);
+            gl.glVertex3f(x+extX, y-extY, 0.25f);
+            gl.glVertex3f(x+extX, y+extY, 0.25f);
+            
+            gl.glEnd();
+            
+//            gl.glEnable(GL.GL_LINE_STIPPLE);
+//            gl.glLineStipple(5, (short)0xAAAA);
+            
+            gl.glLineWidth(1.0f);
+            gl.glBegin(GL.GL_LINES);
+            
+            gl.glColor3f(0.33f, 0.33f, 0.33f);
+            
+            final int STEPS = 20;
+            final float scale = 0.2f*extX;
+            for(int i=2; i<STEPS-1; i++) {
+            
+                final float step = i*extY*2.0f/STEPS;
+                
+                gl.glVertex3f(x-scale, step+y-extY, 0.25f);
+                gl.glVertex3f(x+scale, step+y-extY, 0.25f);
+            
+            }
+            
+            gl.glEnd();  
+            
+            final float top    =        1  * extY*2.0f/STEPS;
+            final float bottom = (STEPS-1) * extY*2.0f/STEPS;
+            
+            gl.glBegin(GL.GL_LINES);
+            
+            gl.glColor3f(0.85f, 0.85f, 0.85f);
+            gl.glVertex3f(x, top+y-extY, 0.25f);
+            gl.glVertex3f(x, bottom+y-extY, 0.25f);
+            
+            gl.glEnd();  
+            
+//            gl.glDisable(GL.GL_LINE_STIPPLE);
+            gl.glPolygonMode(GL.GL_FRONT_AND_BACK, GL.GL_FILL);
+            
+            final float scale2 = 0.5f*extX;
+            
+            final float factor = getFactor();
+            final float value = -extY + (1.0f/STEPS) + factor*(2*(extY-(1.0f/STEPS)));
+            final float step2 = (extX/7.0f);
+            
+            
+            gl.glBegin(GL.GL_QUADS);
+            
+            gl.glColor3f(1.0f, 1.0f, 1.0f);
+            gl.glVertex3f(x-scale2, value+y+step2, 0.25f);
+            gl.glVertex3f(x-scale2, value+y-step2, 0.25f);
+            gl.glVertex3f(x+scale2, value+y-step2, 0.25f);
+            gl.glVertex3f(x+scale2, value+y+step2, 0.25f);
+            
+            gl.glEnd();            
+        }
+        
+        public boolean update(float x, float y) {
+            if(x < this.getX()-this.getExtendX() || x > this.getX()+this.getExtendX()
+            || y < this.getY()-this.getExtendY() || y > this.getY()+this.getExtendY()) {
+                return false;
+            }
+            
+            this.setFactor( ((y-this.getY())+getExtendY()) / this.getHeight() );
+            
+            return true;
+        }
+
+        
     }
     
-    // </editor-fold>
+    // </editor-fold>    
        
     
     /** Creates a new instance of FingerPaint */
@@ -265,9 +499,9 @@ public class FingerPaint {
     public static void main(String... args) throws Exception {
        
         final OSCTrackerClient tracker = new OSCTrackerClient(9000);        
-        //tracker.setTracking(true);
+        tracker.setTracking(true);
         
-        final int COLORS = 10;
+        final int COLORS = 20;
         
         final int[] colors = new int[COLORS];
         
@@ -278,7 +512,15 @@ public class FingerPaint {
 
         
         final List<Brush> brushes = new ArrayList<Brush>();
-       
+        final List<Slider> sliders = new ArrayList<Slider>();  
+        
+        Slider slider;
+        slider = new Slider(-0.4f, -0.0f);
+        slider.setSize(slider.getWidth()*1.5f, slider.getHeight() * 1.5f);
+        sliders.add(slider);
+        slider = new Slider(-0.8f, -0.0f);
+        slider.setSize(slider.getWidth()*1.5f, slider.getHeight() * 1.5f);
+        sliders.add(slider);
         
         // <editor-fold defaultstate="collapsed" desc=" OpenGL ">
         
@@ -298,6 +540,8 @@ public class FingerPaint {
             private float ratio = 1.0f;
             private int color;
             private final float[] bgColor = { 0.4f,0.2f,0.2f };
+            
+            private float opacity = 0.5f;
             
             
             public void init(GLAutoDrawable drawable) {
@@ -348,6 +592,13 @@ public class FingerPaint {
                         final float id = (Float) data[blob+2];
                         ids[blob/3] = (int) id;
                         
+                        for(Slider slider : sliders) {
+                            if(slider.update(x, y)) {
+                                continue;
+                            }
+                        }
+                        
+                        
                         boolean isNew = true;
                         
                         for(Brush brush : brushes) {
@@ -391,6 +642,17 @@ public class FingerPaint {
                 
                 // </editor-fold>
                 
+                if(sliders.size() > 0) {
+                    final float size = sliders.get(0).getFactor();
+                    final float radius = 0.01f + 0.08f * size;
+                    for(Brush brush : brushes) {
+                        brush.setRadius(radius);
+                    }
+                }
+                if(sliders.size() > 1) {
+                    opacity = sliders.get(1).getFactor();
+                }                
+                
                 final GL gl = drawable.getGL();
                 
                 //gl.glClear(GL.GL_COLOR_BUFFER_BIT);
@@ -403,12 +665,12 @@ public class FingerPaint {
                 
                 gl.glBegin(GL.GL_QUADS);
 
-                gl.glColor4f(1.0f,1.0f,1.0f,0.005f);//bgColor[0],bgColor[1],bgColor[2],0.005f);////0.4f,0.2f,0.2f, 0.005f);
+                gl.glColor4f(1.0f,1.0f,1.0f, 0.0025f+ 0.1f*(float)Math.log(opacity+0.5f));//bgColor[0],bgColor[1],bgColor[2],0.005f);////0.4f,0.2f,0.2f, 0.005f);
 
-                gl.glVertex3f(-1.0f,  1.0f, 0.5f);
-                gl.glVertex3f(-1.0f, -1.0f, 0.5f);
-                gl.glVertex3f( 1.0f, -1.0f, 0.5f);
-                gl.glVertex3f( 1.0f,  1.0f, 0.5f);
+                gl.glVertex3f(-1.0f,  1.0f, 0.15f);
+                gl.glVertex3f(-1.0f, -1.0f, 0.15f);
+                gl.glVertex3f( 1.0f, -1.0f, 0.15f);
+                gl.glVertex3f( 1.0f,  1.0f, 0.15f);
 
                 gl.glEnd();
                 
@@ -422,6 +684,10 @@ public class FingerPaint {
                 for(Brush brush : brushes) {                    
                     brush.draw(gl);                    
                 }
+                
+                for(Slider slider : sliders) {                    
+                    slider.draw(gl);                    
+                }                
                 
             }
             
@@ -466,7 +732,6 @@ public class FingerPaint {
         
         final MouseAdapter mouseInput = new MouseAdapter()
         {                        
-            
             private Brush brush;
             private int color = 0;
             
@@ -516,8 +781,8 @@ public class FingerPaint {
                 brush.setLocation(x,y);
             }            
         };
-        canvas.addMouseListener(mouseInput);
-        canvas.addMouseMotionListener(mouseInput);
+        //canvas.addMouseListener(mouseInput);
+        //canvas.addMouseMotionListener(mouseInput);
         
         // </editor-fold>
         
