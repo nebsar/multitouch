8cca87d1a71d763ffd3c8509441dbd7054feb92f
Michael.Nischt <Michael.Nischt@2735173b-5428-0410-ba1d-dbb0dc069ffb>
2007-03-12 14:38:30 +0000

    First step: render everything outside the round table (circle) black, so nothing will be shown by the projector.

---
+++ trunk/demo/java/src/de/telekom/laboratories/multitouch/demo/machina/Main.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2007 Deutsche Telekom AG Laboratories
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package de.telekom.laboratories.multitouch.demo.machina;
+
+/**
+ *
+ * @author Michael Nischt
+ * @version 0.1
+ */
+public class Main {
+    
+    private Main() {}
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Graphics.test();
+    }
+    
+}
