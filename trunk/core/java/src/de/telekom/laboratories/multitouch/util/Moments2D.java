cfc9b6658eff0ef2c49cc26367f27da168a89da3
Michael.Nischt <Michael.Nischt@2735173b-5428-0410-ba1d-dbb0dc069ffb>
2007-04-22 22:38:46 +0000

    Moments2D implementation almost finished.

--- trunk/core/java/src/de/telekom/laboratories/multitouch/util/Moments2D.java
+++ trunk/core/java/src/de/telekom/laboratories/multitouch/util/Moments2D.java
@@ -27,7 +27,6 @@ import java.util.Arrays;
  */
 public class Moments2D {
     
-    private double zero;
     private final double[][] moments;
     
     public Moments2D() {
@@ -35,24 +34,18 @@ public class Moments2D {
     }
     
     public Moments2D(int degree) {
-        //moments = new double[degree+1][];
-        //for(int i=0; i<moments.length; i++) {
-        //    moments[i] = new double[i];
-        //}
-        moments = new double[degree][];
-        for(int i=0; i<moments.length;) {
+        moments = new double[degree+1][];
+        for(int i=0; i<moments.length; ) {
             moments[i] = new double[++i];
         }
     }
     
     public int getDegree() {
-        //return moments.length-1;
-        return moments.length;
+        return moments.length-1;
     }    
 
     public void clear() {
-        //final double zero = 0.0;
-        zero = 0.0;
+        final double zero = 0.0;
         for(double[] moment : moments) {
             Arrays.fill(moment, zero);
         }
@@ -67,32 +60,72 @@ public class Moments2D {
         if(degree > getDegree()) {
             throw new IllegalArgumentException();
         }                
-        //return moments[degree][y] : zero;
-        return (degree == 0) ? moments[degree-1][y] : zero;
-    }            
+        return moments[degree][y];
+    } 
+    
     
     public void from(Shape2D shape) {
                 
         shape.points(new Shape2D.Points() {
+
+           final double[] tmp = new double[getDegree()+1];            
+            
             public void point(double x, double y) { 
                 point(x,y,1.0);
             }
-            public void point(double x, double y, double intensity) { 
-                //moments[0][0] += intensity;
-                zero += intensity;
-                for(int i=1; i<moments.length; i++) {
-                    final double[] ithMoments = moments[i];
-                    
-                    
+            public void point(double x, double y, double intensity) {                                 
+                
+                for(int j=1; j<tmp.length; j++) {
+                    tmp[j] = 0.0;
+                }                
+                moments[0][0] += (tmp[0] = intensity);
+                for(int j=1; j<moments.length; j++) {
+                    final double[] ithMoments = moments[j];
+                    for (int i=ithMoments.length-1; i>0; i--) {
+                        ithMoments[i] += tmp[i] = (tmp[i-1] * y);
+                    }                        
+                    ithMoments[0] += (tmp[0] *= x);
                 }
             }
         });
     }
     
     public static double from(Shape2D shape, int x, int y) {
+        
         final double moment = 0.0;
+        
         
         return moment;
     }
+    
+    public static void main(String... args) throws Exception {
+        Shape2D s = new Shape2D() {
+            public void points(Shape2D.Points points) {
+                points.point(3,2, 4);
+                points.point(4,2, 1);
+                points.point(5,2, 1);
+                points.point(3,3, 2);
+                points.point(4,3, 5);
+                points.point(5,3, 9);
+            }
+
+            public double getMinX() { return 3; }
+            public double getMinY() { return 2; }
+
+            public double getMaxX() { return 5; }
+            public double getMaxY() { return 3; }            
+        };
+        
+        Moments2D m = new Moments2D(3);
+        m.from(s);
+        System.out.printf("S: %f\n", m.at(0,0) );
+        System.out.printf("Sx: %f Sy: %f\n", m.at(1,0), m.at(0,1) );
+        System.out.printf("Sxx: %f Syy %f Sxy %f\n", m.at(2,0), m.at(0,2), m.at(1,1) );
+        System.out.printf("Sxxx: %f Sxxy %f Sxyy %f Syyy %f\n", m.at(3,0), m.at(2,1), m.at(1,2), m.at(0,3) );
+
+        System.out.println();
+        
+        
+    }    
     
 }
