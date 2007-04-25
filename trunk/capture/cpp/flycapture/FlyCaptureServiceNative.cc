e7be8b7c50a89b71254ff9fec20f30fd1d66f4de
Michael.Nischt <Michael.Nischt@2735173b-5428-0410-ba1d-dbb0dc069ffb>
2007-04-25 15:53:25 +0000

    Minor updates part2

--- trunk/capture/cpp/flycapture/FlyCaptureServiceNative.cc
+++ trunk/capture/cpp/flycapture/FlyCaptureServiceNative.cc
@@ -56,7 +56,7 @@ JNIEXPORT jlong JNICALL Java_de_telekom_laboratories_capture_spi_FlyCaptureNativ
        //printf("ERROR: %d < %d", uiSize, index);
        return (jlong) uiSize;
    }
-       
+    
     return (jlong) (arInfo + index)->SerialNumber;
 }
 
@@ -102,14 +102,14 @@ JNIEXPORT jboolean JNICALL Java_de_telekom_laboratories_capture_spi_FlyCaptureNa
         
     jclass videomodeType = env->GetObjectClass(videoMode);
     
-    jmethodID xID      = env->GetMethodID(videomodeType, "getX", "()I");
-    jmethodID yID      = env->GetMethodID(videomodeType, "getY", "()I");    
+    //jmethodID xID      = env->GetMethodID(videomodeType, "getX", "()I");
+    //jmethodID yID      = env->GetMethodID(videomodeType, "getY", "()I");    
     jmethodID widthID  = env->GetMethodID(videomodeType, "getWidth", "()I");
     jmethodID heightID = env->GetMethodID(videomodeType, "getHeight", "()I");
     jmethodID foramtID = env->GetMethodID(videomodeType, "getFormat", "()Lde/telekom/laboratories/capture/VideoMode$Format;");
         
-    unsigned int x      = env->CallIntMethod(videoMode, xID);
-    unsigned int y      = env->CallIntMethod(videoMode, yID);
+    //unsigned int x      = env->CallIntMethod(videoMode, xID);
+    //unsigned int y      = env->CallIntMethod(videoMode, yID);
     unsigned int width  = env->CallIntMethod(videoMode, widthID);
     unsigned int height = env->CallIntMethod(videoMode, heightID); 
     
@@ -121,55 +121,115 @@ JNIEXPORT jboolean JNICALL Java_de_telekom_laboratories_capture_spi_FlyCaptureNa
     
     FlyCapturePixelFormat format = convertFormatIndex(ordinal);
     
-    int mode;
-    for(mode=0; mode<=7; mode++) {
-        
-       bool		  bAvailable;
-       unsigned int	  uiMaxImageSizeCols;
-       unsigned int	  uiMaxImageSizeRows;
-       unsigned int	  uiUnitSizeHorz;
-       unsigned int	  uiUnitSizeVert;
-       unsigned int       uiPixelFormats;
-       
-       const int status = flycaptureQueryCustomImage(context, mode, 
-                                    &bAvailable,
-                                    &uiMaxImageSizeCols,
-                                    &uiMaxImageSizeRows,
-                                    &uiUnitSizeHorz,
-                                    &uiUnitSizeVert,
-                                    &uiPixelFormats);
-        if(status != FLYCAPTURE_OK) {
-            flycaptureDestroyContext( context );
-            return (jboolean) false;
-        }
-        if(!bAvailable) {
-            continue;
-        } else if((x % uiUnitSizeHorz) || (y % uiUnitSizeVert) || (width % uiUnitSizeHorz) || (height % uiUnitSizeVert)) {
-            //printf("%d %d\n", uiUnitSizeHorz, uiUnitSizeVert);
-            continue;
-        } else if((x+width) > uiMaxImageSizeCols || (y+height) > uiMaxImageSizeRows) {
-            printf("%d %d <-> %d %d\n", uiMaxImageSizeCols, uiMaxImageSizeRows, (x+width), (y+height));
-            continue;
-        } else if((format & uiPixelFormats) == 0) {
-            continue;
-        }
-       
-        if(flycaptureStartCustomImage(context, mode, x, y, width, height, 100.0f, format) != FLYCAPTURE_OK) {                    
-            //printf("cannot start");
-            continue;
-        } else {
-            //printf("x: %d, y: %d, width: %d, height: %d\n", x, y, width, height);
-            connections[serial] = context;      
-            return (jboolean) true;
-        }       
+//    int mode;
+//    for(mode=0; mode<=7; mode++) {
+//        
+//       bool		  bAvailable;
+//       unsigned int	  uiMaxImageSizeCols;
+//       unsigned int	  uiMaxImageSizeRows;
+//       unsigned int	  uiUnitSizeHorz;
+//       unsigned int	  uiUnitSizeVert;
+//       unsigned int       uiPixelFormats;
+//       
+//       const int status = flycaptureQueryCustomImage(context, mode, 
+//                                    &bAvailable,
+//                                    &uiMaxImageSizeCols,
+//                                    &uiMaxImageSizeRows,
+//                                    &uiUnitSizeHorz,
+//                                    &uiUnitSizeVert,
+//                                    &uiPixelFormats);
+//        if(status != FLYCAPTURE_OK) {
+//            flycaptureDestroyContext( context );
+//            return (jboolean) false;
+//        }
+//        if(!bAvailable) {
+//            continue;
+//        } else if((x % uiUnitSizeHorz) || (y % uiUnitSizeVert) || (width % uiUnitSizeHorz) || (height % uiUnitSizeVert)) {
+//            //printf("%d %d\n", uiUnitSizeHorz, uiUnitSizeVert);
+//            continue;
+//        } else if((x+width) > uiMaxImageSizeCols || (y+height) > uiMaxImageSizeRows) {
+//            printf("%d %d <-> %d %d\n", uiMaxImageSizeCols, uiMaxImageSizeRows, (x+width), (y+height));
+//            continue;
+//        } else if((format & uiPixelFormats) == 0) {
+//            continue;
+//        }
+//       
+//        if(flycaptureStartCustomImage(context, mode, x, y, width, height, 100.0f, format) != FLYCAPTURE_OK) {                    
+//            //printf("cannot start");
+//            continue;
+//        } else {
+//            //printf("x: %d, y: %d, width: %d, height: %d\n", x, y, width, height);
+//            connections[serial] = context;      
+//            return (jboolean) true;
+//        }       
+//    }
+    
+    if(format != FLYCAPTURE_MONO8) {
+        flycaptureDestroyContext( context );
+        return (jboolean) false;
     }
     
-    //if(flycaptureStart(context, FLYCAPTURE_VIDEOMODE_640x480Y8, FLYCAPTURE_FRAMERATE_120) == FLYCAPTURE_OK)
-    //{
-    //    connections[serial] = context;   
-    //    return (jboolean) true;
-    //}    
+    FlyCaptureVideoMode camera1VideoMode;
+    if(width == 640 && height == 480) {
+        camera1VideoMode = FLYCAPTURE_VIDEOMODE_640x480Y8;
+    } else if(width == 1024 && height == 768) {
+        camera1VideoMode = FLYCAPTURE_VIDEOMODE_1024x768Y8;
+    } else {
+        flycaptureDestroyContext( context );                
+        return (jboolean) false;
+    }
     
+    bool supported = false;
+    FlyCaptureFrameRate frameRate;
+    
+    if( !supported && (flycaptureCheckVideoMode(context, camera1VideoMode, FLYCAPTURE_FRAMERATE_30, &supported ) != FLYCAPTURE_OK)) {
+        flycaptureDestroyContext( context );        
+        return (jboolean) false;
+    } else {
+        frameRate = FLYCAPTURE_FRAMERATE_30;
+    }
+
+    if( !supported && (flycaptureCheckVideoMode(context, camera1VideoMode, FLYCAPTURE_FRAMERATE_15, &supported ) != FLYCAPTURE_OK)) {
+        flycaptureDestroyContext( context );        
+        return (jboolean) false;
+    } else {
+        frameRate = FLYCAPTURE_FRAMERATE_15;
+    }     
+    
+    
+//    FlyCaptureVideoMode vMode = FLYCAPTURE_VIDEOMODE_ANY;
+//    FlyCaptureFrameRate fRate = FLYCAPTURE_FRAMERATE_ANY;    
+//    if(flycaptureGetCurrentVideoMode(context, &vMode, &fRate) == FLYCAPTURE_OK) {
+//        printf("VideoMode: %d + FrameRate: %d\n", vMode, fRate);
+//    } else {
+//        flycaptureDestroyContext( context );        
+//        return (jboolean) false;        
+//    }        
+
+    
+    if(flycaptureStart(context, camera1VideoMode, frameRate ) == FLYCAPTURE_OK)
+    {         
+        connections[serial] = context;        
+        return (jboolean) true;
+    }               
+    
+//    FlyCaptureVideoMode camera1VideoMode = FLYCAPTURE_VIDEOMODE_ANY;
+//    FlyCaptureFrameRate frameRate = FLYCAPTURE_FRAMERATE_ANY;    
+//    if(flycaptureStart(context, camera1VideoMode, frameRate ) == FLYCAPTURE_OK)
+//        if(flycaptureGetCurrentVideoMode(context, &camera1VideoMode, &frameRate) != FLYCAPTURE_OK) {
+//            flycaptureStop( context );
+//            flycaptureDestroyContext( context );
+//        } else {
+//            jmethodID setWidthID  = env->GetMethodID(videomodeType, "setWidth", "(I)");
+//            jmethodID setHeightID = env->GetMethodID(videomodeType, "setHeight", "(I)");
+////            jmethodID setForamtID = env->GetMethodID(videomodeType, "setFormat", "(Lde/telekom/laboratories/capture/VideoMode$Format;)");            
+//            
+//            env->CallVoidMethod(videoMode, setWidthID, 
+//            env->CallVoidMethod(videoMode, setHeightID, 
+//        }
+//        connections[serial] = context;        
+//        return (jboolean) true;
+//    }        
     
     flycaptureDestroyContext( context );
     return (jboolean) false;
