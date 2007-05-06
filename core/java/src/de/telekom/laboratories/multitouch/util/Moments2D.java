327d1fc5e919fb62cae13512047694ab13fbab8d
Michael.Nischt <Michael.Nischt@2735173b-5428-0410-ba1d-dbb0dc069ffb>
2007-05-06 14:12:42 +0000

    Add missing license info

--- core/java/src/de/telekom/laboratories/multitouch/util/Moments2D.java
+++ core/java/src/de/telekom/laboratories/multitouch/util/Moments2D.java
@@ -1,10 +1,19 @@
 /*
- * Moments2D.java
+ * Copyright (C) 2007 Deutsche Telekom AG Laboratories
  *
- * Created on May 5, 2007, 4:16:50 PM
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
 package de.telekom.laboratories.multitouch.util;
