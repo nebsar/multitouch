9b5e0c72e8c09c4e3aea167c013556dc800b12c2
Michael.Nischt <Michael.Nischt@2735173b-5428-0410-ba1d-dbb0dc069ffb>
2007-05-07 12:55:18 +0000

    Prevented jvm error by catching exceptions during the shutdown-hook.

--- trunk/capture/java/src/de/telekom/laboratories/capture/spi/FlyCaptureService.java
+++ trunk/capture/java/src/de/telekom/laboratories/capture/spi/FlyCaptureService.java
@@ -81,7 +81,9 @@ public class FlyCaptureService extends CaptureService
             {
                 public void run()
                 {
+                    try {
                     FlyCaptureNative.releaseDeviceHandle(handle);
+                    } catch(Exception e) {}
                 }
             };
 
