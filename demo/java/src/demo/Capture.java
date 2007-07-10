646471370c82063e33f9e743f6cf3c8f741333aa
Michael.Nischt <Michael.Nischt@2735173b-5428-0410-ba1d-dbb0dc069ffb>
2007-07-10 10:02:59 +0000

    Cleanup: Demo-Capture

--- demo/java/src/demo/Capture.java
+++ demo/java/src/demo/Capture.java
@@ -1,10 +1,19 @@
 /*
- * Capture.java
- * 
- * Created on Jul 10, 2007, 9:59:34 AM
- * 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2007 Deutsche Telekom AG Laboratories
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
 package demo;
@@ -17,8 +26,7 @@ import static demo.Capture.Flip.HORIZONTAL;
 import static demo.Capture.Flip.VERTICAL;
 
 /**
- *
- * @author gestalt
+ * @author Michael Nischt
  * @version 0.1
  */
 abstract public class Capture 
