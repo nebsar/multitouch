0463432ae63c2d5ff8ee88f939688698672ebeca
Michael.Nischt <Michael.Nischt@2735173b-5428-0410-ba1d-dbb0dc069ffb>
2007-11-20 20:52:16 +0000

    Added WorldWind demo draft.

---
+++ demo/java/src/demo/worldwind/AWT1UpTouch.java
@@ -0,0 +1,179 @@
+/*
+Copyright (C) 2001, 2006 United States Government
+as represented by the Administrator of the
+National Aeronautics and Space Administration.
+All Rights Reserved.
+*/
+package demo.worldwind;
+
+import gov.nasa.worldwind.*;
+import gov.nasa.worldwind.avlist.AVKey;
+import gov.nasa.worldwind.avlist.AVList;
+import gov.nasa.worldwind.awt.WorldWindowGLCanvas;
+import gov.nasa.worldwind.event.*;
+import gov.nasa.worldwind.examples.*;
+import gov.nasa.worldwind.formats.georss.GeoRSSParser;
+import gov.nasa.worldwind.geom.*;
+import gov.nasa.worldwind.globes.Globe;
+import gov.nasa.worldwind.layers.*;
+import gov.nasa.worldwind.layers.Earth.*;
+import gov.nasa.worldwind.pick.PickedObjectList;
+import gov.nasa.worldwind.render.*;
+import gov.nasa.worldwind.view.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.Map.Entry;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.swing.*;
+import javax.xml.parsers.*;
+import java.awt.*;
+import java.awt.font.*;
+import java.io.*;
+import java.util.*;
+import java.util.List;
+
+/**
+ * @author Tom Gaskins
+ * @version $Id: AWT1Up.java 3209 2007-10-06 21:57:53Z tgaskins $
+ */
+public class AWT1UpTouch
+{
+    private static class AWT1UpFrame extends javax.swing.JFrame
+    {
+        private boolean fullscreen = true;
+        private int screen = 0;
+                
+        StatusBar statusBar;
+        JLabel cursorPositionDisplay;
+        WorldWindowGLCanvas wwd;
+
+        public AWT1UpFrame()
+        {
+            try
+            {                
+                System.out.println(gov.nasa.worldwind.Version.getVersion());
+
+                wwd = new gov.nasa.worldwind.awt.WorldWindowGLCanvas();
+                wwd.setPreferredSize(new java.awt.Dimension(800, 600));
+                this.getContentPane().add(wwd, java.awt.BorderLayout.CENTER);
+
+                this.statusBar = new StatusBar();
+                this.getContentPane().add(statusBar, BorderLayout.PAGE_END);                                
+                
+                // <editor-fold defaultstate="collapsed" desc=" Fullscreen ">
+
+                final GraphicsDevice[] graphicsDevices = GraphicsEnvironment.getLocalGraphicsEnvironment ().getScreenDevices ();
+                final GraphicsDevice   graphicsDevice  = graphicsDevices[Math.max (0, screen = Math.min (screen, graphicsDevices.length-1))];                
+                                
+                if(fullscreen)
+                {// && device.isFullScreenSupported()) {
+                    try
+                    {
+                        setUndecorated (true);
+                        graphicsDevice.setFullScreenWindow (this);
+                    }
+                    catch(Exception e)
+                    {
+                        System.err.println(e.getMessage());
+                        e.printStackTrace();
+                        graphicsDevice.setFullScreenWindow (null);
+                        fullscreen = false;
+                    }
+                }
+
+                if(!fullscreen)
+                {
+                    java.awt.Dimension prefSize = this.getPreferredSize();
+                    java.awt.Dimension parentSize;
+                    java.awt.Point parentLocation = new java.awt.Point(0, 0);
+                    parentSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+                    int x = parentLocation.x + (parentSize.width - prefSize.width) / 2;
+                    int y = parentLocation.y + (parentSize.height - prefSize.height) / 2;
+                    this.setLocation(x, y);
+                    this.setResizable(true);
+                }
+
+                // </editor-fold>
+                
+                Model model = (Model) WorldWind.createConfigurationComponent(AVKey.MODEL_CLASS_NAME);
+                wwd.setModel(model);
+                
+                // Forward events to the status bar to provide the cursor position info.
+                this.statusBar.setEventSource(wwd);
+                
+                
+                final OrbitView view = (OrbitView)wwd.getView();
+                final TouchInput touchInput = new TouchInput(view, model);
+                
+                Thread t = new Thread("Touch.Inputs")
+                {        
+                    private boolean even = true;
+                    
+                    private void frame()
+                    {
+                        System.out.println("frame");
+                        if(even)
+                        {
+                            touchInput.advanceFrame(Collections.singleton(new Touch(0, -5)).iterator());
+                        }
+                        else
+                        {
+                            touchInput.advanceFrame(Collections.singleton(new Touch(0, +5)).iterator());
+                        }
+                        even = !even;                        
+                    }
+                    
+                    @Override public void run ()
+                    {
+                        while(true)
+                        {
+                            try
+                            {
+                                sleep(1000);   
+                            }
+                            catch(InterruptedException ie) {}
+
+                            EventQueue.invokeLater(new Runnable() 
+                            {
+                                public void run() 
+                                {
+                                    frame();
+                                }
+                            });                        
+                        }
+                        
+                    }
+                };
+                t.setDaemon (true);
+                t.setPriority (Thread.NORM_PRIORITY);//(Thread.NORM_PRIORITY + Thread.MAX_PRIORITY)/2);
+                t.start ();
+                
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception
+    {
+        System.out.println("Java run-time version: " + System.getProperty("java.version"));
+
+        EventQueue.invokeAndWait(new Runnable() 
+        {            
+            public void run() 
+            {
+                AWT1UpFrame frame = new AWT1UpFrame();
+                frame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+            }
+        });
+        
+        
+        //for(int i=0; i)
+    }
+}
---
+++ demo/java/src/demo/worldwind/TouchInput.java
@@ -0,0 +1,184 @@
+/*
+ * Copyright (C) 2007 Deutsche Telekom AG Laboratories
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package demo.worldwind;
+
+import de.telekom.laboratories.tracking.Matcher;
+import de.telekom.laboratories.tracking.Observer;
+import de.telekom.laboratories.tracking.Tracker;
+import de.telekom.laboratories.tracking.Trackers;
+import gov.nasa.worldwind.Model;
+import gov.nasa.worldwind.avlist.AVKey;
+import gov.nasa.worldwind.geom.Angle;
+import gov.nasa.worldwind.geom.Quaternion;
+import gov.nasa.worldwind.geom.Vec4;
+import gov.nasa.worldwind.globes.Globe;
+import gov.nasa.worldwind.view.OrbitView;
+import gov.nasa.worldwind.view.View;
+
+import java.util.Iterator;
+
+/**
+ * @author Michael Nischt
+ * @version 0.1
+ */
+public class TouchInput
+{
+    // <editor-fold defaultstate="collapsed" desc=" Attributes ">
+    
+    private OrbitView view;
+    private Model model;
+    
+    // </editor-fold>
+    
+    // <editor-fold defaultstate="collapsed" desc=" Initializers ">
+    
+    public TouchInput(OrbitView view, Model model)
+    {
+        if(view == null || model == null)
+            throw new NullPointerException();
+        
+        this.view = view;
+        this.model = model;
+    }
+    
+    // </editor-fold>
+        
+    
+    // <editor-fold defaultstate="collapsed" desc=" DragUtil ">
+    
+    private Angle computeLatOrLonChange(View view, Globe globe, double amount)
+    {
+        Vec4 eye = view.getEyePoint();
+        if (eye == null)
+            return null;
+
+        double normAlt = (eye.getLength3() / globe.getMaximumRadius()) - 1.0;
+        if (normAlt < 0.0)
+            normAlt = 0.0;
+        else if (normAlt > 1.0)
+            normAlt = 1.0;
+
+        double coeff = (0.00001 * (1.0 - normAlt)) + (0.2 * normAlt);
+
+        return Angle.fromDegrees(coeff * amount);
+    }    
+    
+    private void drag(java.awt.Point move) 
+    {        
+        final Globe globe = model.getGlobe();
+        if (globe == null)
+            return;
+                
+        Angle forwardAngle = this.computeLatOrLonChange(this.view, globe, move.y);
+        Angle rightAngle = this.computeLatOrLonChange(this.view, globe, -move.x);
+        Quaternion forwardQuat = this.view.createRotationForward(forwardAngle);
+        Quaternion rightQuat = this.view.createRotationRight(rightAngle);
+        Quaternion quaternion = forwardQuat.multiply(rightQuat);
+                
+        view.setRotation(view.getRotation().multiply(quaternion));
+        
+        if (view.hasStateIterator())
+            view.stopStateIterators();
+
+        view.firePropertyChange(AVKey.VIEW, null, view);        
+    }        
+    
+
+    // </editor-fold>
+       
+    // <editor-fold defaultstate="collapsed" desc=" Tracking ">
+    
+    private final Tracker<Touch> tracker = Trackers.bestMatch ( new Matcher<Touch,Double>()
+    {
+        @Override
+        public Double match (Touch a, Touch b)
+        {
+            final double distance = Touch.Utils.distance (a, b);
+            return -distance;//return ( distance <= 0.1 ) ? distance : null;
+        }
+
+        @Override
+        public int compare (Double a, Double b)
+        {
+            return b.compareTo (a);
+        }
+    }, true);
+    
+    public void advanceFrame(Iterator<? extends Touch> touchIt)
+    {
+        // <editor-fold defaultstate="collapsed" desc=" Vision ">
+                    
+//        try
+//        {                
+//            final Thread t = new Thread ("Multitouch.Demo.Capture")
+//            {                    
+//                //final private TLCapture capture = new TLCapture (scene, width, height);
+//
+//                //final private byte[]  image  = new byte[width*height];
+//
+//                @Override public void run ()
+//                {
+//                    //Capture device = Capture.startDevice(width, height);
+//                    while(this == currentThread())
+//                    {
+//                        //device.capture (image, Capture.Flip.VERTICAL);//allOf(Capture.Flip.class));
+//                        //capture.capture(image);
+//                    }
+//                }
+//            };
+//            t.setDaemon (true);
+//            t.setPriority (Thread.NORM_PRIORITY);//(Thread.NORM_PRIORITY + Thread.MAX_PRIORITY)/2);
+//            t.start ();
+//
+//
+//        }
+//        catch(Exception e)
+//        {
+//            throw new RuntimeException ("Could not initialize vision", e);
+//        }
+            
+        // </editor-fold>
+        
+        while(touchIt.hasNext ())
+        {
+            final Touch touch = touchIt.next ();
+            tracker.track (touch);
+        }
+        
+        final java.awt.Point move = new java.awt.Point(0,0);
+        
+        tracker.nextFrame(new Observer.Adapter<Touch>() 
+        {
+            @Override
+            public void updatedTracking(Touch last, Touch current) 
+            {
+                System.out.println("l: " + last.getX() + " " + last.getY());
+                System.out.println("c: " + current.getX() + " " + current.getY());
+                move.x += current.getX() - last.getX();
+                move.y += current.getY() - last.getY();
+            }
+        });       
+               
+        
+        System.out.println(move.x + " " + move.y);
+        drag(move);        
+    }
+    
+    // </editor-fold>    
+}
