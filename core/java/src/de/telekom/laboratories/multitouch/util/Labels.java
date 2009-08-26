6eb441e317b1646856cf36c3fdc13e18f980fb08
Michael.Nischt <Michael.Nischt@2735173b-5428-0410-ba1d-dbb0dc069ffb>
2009-08-26 12:42:12 +0000

    Another small for for Labels.java and added 3 more test cases to LabelsTest.java

--- core/java/src/de/telekom/laboratories/multitouch/util/Labels.java
+++ core/java/src/de/telekom/laboratories/multitouch/util/Labels.java
@@ -18,25 +18,34 @@
 
 package de.telekom.laboratories.multitouch.util;
 
+import static java.lang.Math.*;
+
 /**
  *
  * @author Michael Nischt
  * @version 0.1
  */
-public class Labels {
-    
+public class Labels
+{
+    public static final int MIN_X = 0;
+    public static final int MIN_Y = 1;
+    // including index
+    public static final int MAX_X = 2;
+    public static final int MAX_Y = 3;
+
     // <editor-fold defaultstate="collapsed" desc=" Attributes ">
-    
-    private final int[][] image;    
+
+    private final int[][] image;
     private final int[] labels;
-    
+
     private int current;
-    
+
     // </editor-fold>
 
     // <editor-fold defaultstate="collapsed" desc=" Initializers ">
-    
-    public Labels(int[][] image) {
+
+    public Labels(int[][] image)
+    {
         if(image.length < 1) {
             throw new IllegalArgumentException();
         }
@@ -47,37 +56,38 @@ public class Labels {
             }
         }
         this.image = image.clone();
-        this.labels = new int[ (width*image.length) / 2 ]; // max possible unique labels
+        this.labels = new int[ round(width*image.length / 2.0f) + 1]; // max possible unique labels
     }
-    
+
     // </editor-fold>
-    
+
     // <editor-fold defaultstate="collapsed" desc=" Properties ">
-    
+
 //    public int getCount() {
 //        return current;
 //    }
-    
+
     // </editor-fold>
-    
+
     // <editor-fold defaultstate="collapsed" desc=" Methods ">
-    
+
     public int count() {
         current = 0;
-        
+
         int[] line = image[0];
-        
+
         // first pixel
         int pixel = line[0] = (line[0] != 0) ? apply() : 0;
-        
+
         // firstLine
         for(int i=1; i<line.length; i++) {
             pixel = line[i] = (line[i] != 0) ? apply(pixel) : 0;
         }
-        
-        for(int j=1; j<image.length; j++) {            
+
+        for(int j=1; j<image.length; j++) 
+        {
             final int[] line2 = image[j];
-            
+
             // first pixel
             pixel = line2[0] = (line2[0] != 0) ? apply(line[0]) : 0;
 
@@ -85,89 +95,85 @@ public class Labels {
             for(int i=1; i<line.length; i++) {
                 pixel = line2[i] = (line2[i] != 0) ? apply(line[i], pixel) : 0;
             }
-                        
+
             line = line2;
         }
-        
+
         int count = 0;
         for(int i=1; i<=current; i++) {
             if(i != labels[i]) {
                 labels[i] = labels[ labels[i] ];
             } else {
                 count++;
-                labels[i] = count;                
+                labels[i] = count;
             }
         }
         current = count;
-        
+
         for(int j=0; j<image.length; j++) {
             line = image[j];
             for(int i=0; i<line.length; i++) {
                 line[i] = labels[line[i]];
             }
         }
-        return count;        
-    }    
-    
-    public int[][] bounds() {                
-        final int count = count();
-        final int minX = 0, minY = 1, maxX = 2, maxY = 3;
-        
+        return count;
+    }
+
+    public int[][] bounds() {
+        final int count = count();        
+
         final int[][] bounds = new int[count][4];
         for(int i=0; i<bounds.length; i++) {
             final int[] b = bounds[i];
-            b[minX] = b[minY] = Integer.MAX_VALUE;
-            b[maxX] = b[maxY] = Integer.MIN_VALUE;
+            b[MIN_X] = b[MIN_Y] = Integer.MAX_VALUE;
+            b[MAX_X] = b[MAX_Y] = Integer.MIN_VALUE;
         }
 
         for(int y=0; y<image.length; y++) {
             final int[] line = image[y];
             for(int x=0; x<line.length; x++) {
                 final int pixel = line[x];
-                if(pixel > 0) {                    
+                if(pixel > 0) {
                     final int[] b = bounds[pixel-1];
-                    
-                    if(x < b[minX]) { b[minX] = x; }
-                    if(y < b[minY]) { b[minY] = y; }
-                    if(x > b[maxX]) { b[maxX] = x; }
-                    if(y > b[maxY]) { b[maxY] = y; }
-                    //b[minX] = Math.min(b[minX], x);
-                    //b[maxX] = Math.max(x, b[maxX]);
-                    //b[minY] = Math.min(b[minY], y);
-                    //b[maxY] = Math.max(y, b[maxY]);
+
+                    if(x < b[MIN_X]) { b[MIN_X] = x; }
+                    if(y < b[MIN_Y]) { b[MIN_Y] = y; }
+                    if(x > b[MAX_X]) { b[MAX_X] = x; }
+                    if(y > b[MAX_Y]) { b[MAX_Y] = y; }
                 }
             }
-        }        
-        
+        }
+
         return bounds;
     }
-    
+
     private int apply() {
-        ++current;        
+        ++current;
         return (labels[current] = current);
     }
     private int apply(int a) {
         return (a != 0) ? labels[a] : apply();
     }
-    private int apply(int a, int  b) 
-	{        
+    private int apply(int a, int  b)
+    {
         if(a != 0 && b != 0) {
             return apply( union(a,b) );
-        } else if( a != 0) {            
+        } else if( a != 0) {
             return apply(a);
         } else if(b != 0) {
             return apply(b);
         } else {
             return apply();
         }
-    }    
-
-    private int union(int a, int b) 
-    {
-        int labelMin = Math.min(labels[a], labels[b]);
-        int valueMin = Math.min(a, b);
-        return labels[a] = labels[b] = Math.min(labelMin, valueMin);
     }
-    
+
+    private int union(int a, int b)
+    {
+        int labelMin = min(labels[a], labels[b]);
+        int valueMin = min(a, b);
+        return labels[a] = labels[b] = min(labelMin, valueMin);
+    }
+
     // </editor-fold>
 }
+
--- core/java/test/de/telekom/laboratories/multitouch/util/LabelsTest.java
+++ core/java/test/de/telekom/laboratories/multitouch/util/LabelsTest.java
@@ -23,9 +23,7 @@ import junit.framework.JUnit4TestAdapter;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-
-
-
+
 /**
  *
  * @author Michael Nischt
@@ -185,4 +183,355 @@ public class LabelsTest {
     }    
     
     // </editor-fold>
+}import org.junit.Test;
+import static org.junit.Assert.*;
+import static java.util.Arrays.deepEquals;
+
+public class LabelsTest
+{
+    interface Example
+    {
+        int[][] image();
+
+        int[][] bounds();
+    }
+    
+    Example[] examples =
+    {
+        // alternating
+        new Example()
+        {
+            private final int[][] image =
+            {
+                {
+                    1, 0, 2, 0, 3
+                },
+                {
+                    0, 4, 0, 5, 0
+                },
+                {
+                    6, 0, 7, 0, 8
+                },
+                {
+                    0, 9, 0, 10, 0
+                },
+                {
+                    11, 0, 12, 0, 13
+                },
+            };
+            private final int[][] bounds =
+            {
+                //
+                {
+                    0, 0, 0, 0
+                },
+                {
+                    2, 0, 2, 0
+                },
+                {
+                    4, 0, 4, 0
+                },
+                //
+                {
+                    1, 1, 1, 1
+                },
+                {
+                    3, 1, 3, 1
+                },
+                //
+                {
+                    0, 2, 0, 2
+                },
+                {
+                    2, 2, 2, 2
+                },
+                {
+                    4, 2, 4, 2
+                },
+                //
+                {
+                    1, 3, 1, 3
+                },
+                {
+                    3, 3, 3, 3
+                },
+                //
+                {
+                    0, 4, 0, 4
+                },
+                {
+                    2, 4, 2, 4
+                },
+                {
+                    4, 4, 4, 4
+                },
+            };
+
+            public int[][] image()
+            {
+                return image;
+            }
+
+            public int[][] bounds()
+            {
+                return bounds;
+            }
+        },
+        // U S A
+        new Example()
+        {
+            private final int[][] image =
+            {
+                {
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+                },
+                {
+                    0, 1, 0, 1, 0, 2, 2, 2, 0, 3, 3, 3, 0
+                },
+                {
+                    0, 1, 0, 1, 0, 2, 0, 0, 0, 3, 0, 3, 0
+                },
+                {
+                    0, 1, 0, 1, 0, 2, 2, 2, 0, 3, 3, 3, 0
+                },
+                {
+                    0, 1, 0, 1, 0, 0, 0, 2, 0, 3, 0, 3, 0
+                },
+                {
+                    0, 1, 1, 1, 0, 2, 2, 2, 0, 3, 0, 3, 0
+                },
+                {
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+                },
+            };
+            private final int[][] bounds =
+            {
+                {
+                    1, 1, 3, 5
+                },
+                {
+                    5, 1, 7, 5
+                },
+                {
+                    9, 1, 11, 5
+                },
+            };
+
+            public int[][] image()
+            {
+                return image;
+            }
+
+            public int[][] bounds()
+            {
+                return bounds;
+            }
+        },
+        // inner dot in 0
+        new Example()
+        {
+            private final int[][] image =
+            {
+                {
+                    0, 0, 0, 0, 0, 0, 0
+                },
+                {
+                    0, 1, 1, 1, 1, 1, 0
+                },
+                {
+                    0, 1, 0, 0, 0, 1, 0
+                },
+                {
+                    0, 1, 0, 2, 0, 1, 0
+                },
+                {
+                    0, 1, 0, 0, 0, 1, 0
+                },
+                {
+                    0, 1, 1, 1, 1, 1, 0
+                },
+                {
+                    0, 0, 0, 0, 0, 0, 0
+                },
+            };
+            private final int[][] bounds =
+            {
+                {
+                    1, 1, 5, 5
+                },
+                {
+                    3, 3, 3, 3
+                },
+            };
+
+            public int[][] image()
+            {
+                return image;
+            }
+
+            public int[][] bounds()
+            {
+                return bounds;
+            }
+        },
+        // failed once #1
+        new Example()
+        {
+            private final int[][] image =
+            {
+                {
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+                },
+                {
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0
+                },
+                {
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0
+                },
+                {
+                    0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0
+                },
+                {
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+                },
+            };
+            private final int[][] bounds =
+            {
+                {
+                    2, 1, 16, 3,
+                },
+            };
+
+            public int[][] image()
+            {
+                return image;
+            }
+
+            public int[][] bounds()
+            {
+                return bounds;
+            }
+        },
+        // failed once #2
+        new Example()
+        {
+            private final int[][] image =
+            {
+                {
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+                },
+                {
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0
+                },
+                {
+                    0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0
+                },
+                {
+                    0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0
+                },
+                {
+                    0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0
+                },
+                {
+                    0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0
+                },
+                {
+                    0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0
+                },
+                {
+                    0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0
+                },
+                {
+                    0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0
+                },
+                {
+                    0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0
+                },
+                {
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+                },
+            };
+            private final int[][] bounds =
+            {
+                {
+                    2, 1, 12, 9,
+                },
+            };
+
+            public int[][] image()
+            {
+                return image;
+            }
+
+            public int[][] bounds()
+            {
+                return bounds;
+            }
+        }
+    };
+
+    @Test
+    public void testCount()
+    {
+        for (final Example example : examples)
+            testCount(example.image(), example.bounds().length);
+    }
+
+    void testCount(int[][] image, int count)
+    {
+
+        final int[][] copy = image.clone();
+        for (int i = 0; i < copy.length; i++)
+        {
+            copy[i] = image[i].clone();
+        }
+
+        final Labels labels = new Labels(copy);
+        final int c = labels.count();
+        assertEquals("Number of labels is wrong", c, count);
+        assertTrue(
+            "Update doesn't behave as identity for a neutral image",
+            deepEquals(image, copy));
+    }
+
+    @Test
+    public void testBounds()
+    {
+        for (final Example example : examples)
+            testBounds(example.image(), example.bounds());
+    }
+
+    void testBounds(int[][] image, int[][] bounds)
+    {
+
+        final int[][] copy = image.clone();
+        for (int i = 0; i < copy.length; i++)
+        {
+            copy[i] = image[i].clone();
+        }
+
+        Labels labels;
+
+        labels = new Labels(copy);
+        final int[][] b = labels.bounds();
+
+        assertTrue("Bounds do not match", deepEquals(bounds, b));
+
+        assertTrue(
+            "Update doesn't behave as identity for a neutral image",
+            deepEquals(image, copy));
+    }
+
+    static void print(int[][] image)
+    {
+        for (int[] line : image)
+        {
+            for (int pixel : line)
+            {
+                System.out.printf("%4d ", pixel);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
 }
